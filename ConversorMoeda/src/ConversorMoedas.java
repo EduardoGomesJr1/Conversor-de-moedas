973742afc29ae8b5d708dacb64ee0308f80cd6fc
Eduardo Gomes Jr <eduardo.gnjr@gmail.com>
2023-08-19 14:49:34 -0300

    Update passo 2

--- ConversorMoeda/src/ConversorMoedas.java
+++ ConversorMoeda/src/ConversorMoedas.java
@@ -15,17 +15,69 @@ public class ConversorMoedas {
                 "Sair"
         };
 
-        int escolha = JOptionPane.showOptionDialog(null, "Escolha uma opção:", "Conversor de Opções",
-                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
+        String escolha = (String) JOptionPane.showInputDialog(
+                null,
+                "Escolha uma opção:",
+                "Conversor de Moedas",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                opcoes,
+                opcoes[0]
+        );
 
-        if (escolha == 0) {
-            //chamar a função de conversão de moedas aqui
-        } else if (escolha == 1) {
-            // Chamar a função de conversão de temperatura aqui
-        } else if (escolha == 2 || escolha == JOptionPane.CLOSED_OPTION) {
-            System.exit(0);
+        if(escolha != null) {
+            switch (escolha.charAt(0)) {
+                case '1':
+                    // chame a função para a conversão de moedas aqui
+                    break;
+                case '2':
+                    // chame a função para outra opção de convesão aqui
+                    break;
+                case '3':
+                    JOptionPane.showMessageDialog(null, "Obrigado por usar o Conversor de Moedas!", "Conversor de Moedas", JOptionPane.INFORMATION_MESSAGE);
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.", "Erro", JOptionPane.ERROR_MESSAGE);
+                    exibirMenu();
+            }
         }
 
     }
-    // Aqui pode definir as outras funções de conversão e opções necessárias
+    public static  void realizarConversaoDeMoedas() {
+        String[] moedas = {"Real (BRL", "Dólar (USD)", "Euro (EUR)"}; //adicionar mais moedas
+
+        String moedaEscolhida = (String) JOptionPane.showInputDialog(
+                null,
+                "Escolha a moeda de conversão:",
+                "Conversor de Moedas - Escolha a Moeda",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                moedas,
+                moedas[0]
+        );
+
+        if (moedaEscolhida != null) {
+            double valor = 0.0;
+            boolean entradaValida = false;
+
+            while (!entradaValida) {
+                String valorStr = JOptionPane.showInputDialog(
+                        null,
+                        "Digite o valor a ser convertido",
+                        "Conversor de Moedas - Digite o Valor",
+                        JOptionPane.PLAIN_MESSAGE
+                );
+
+                try {
+                    valor = Double.parseDouble(valorStr);
+                    entradaValida = true;
+                } catch (NumberFormatException e ) {
+                    JOptionPane.showInputDialog(null, "Valor inválido. Digite um valor válido.", "Erro", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            // Agora pode realizar a conversão com o valor inserido
+            // e a moeda escolhida pelo usuário
+        }
+    }
 }
